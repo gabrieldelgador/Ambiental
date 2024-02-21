ca4d8d0176484872461efa554410a1349149554b
Gabo <gdelgado@usm.cl>
2024-02-21 16:28:38 -0300

    superusario

--- analisis/models.py
+++ analisis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here. 
 class Rios(models.Model):
-    name=models.CharField(max_lenght=200)
+    name=models.CharField(max_length=200)
     latitud=models.DecimalField(max_digits=9, decimal_places=6)
     logitud=models.DecimalField(max_digits=9, decimal_places=6)
     
